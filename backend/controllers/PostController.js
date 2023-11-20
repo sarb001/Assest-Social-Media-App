db4880b0b974eb3e89120a58f052217a821cd97f
SARB <mrsinghbusiness05@gmail.com>
2023-11-21 04:21:34 +0530

    post Liked/Unliked backend done

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -18,7 +18,7 @@ exports.CreatePost = async (req,res) => {
 
         user.posts.push(post._id)   // post pushed to specific logged user account
         await user.save();
-        
+
         res.status(201).json({
             success: true,
             post: post,
@@ -27,6 +27,51 @@ exports.CreatePost = async (req,res) => {
         res.status(500).json({
             success : false,
             message : error.message
+        })
+    }
+}
+
+// check if post is liked then unlike it 
+// if not  then like it now  
+
+
+exports.LikeandUnlikePost = async(req,res) =>{
+    //check if already liked it or not ?
+    try {
+            // params means specific postid
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({
+            success : false,
+            message : " Post not Found ",
+            })
+        }
+
+        if(post.likes.includes(req.user._id)){          // check userid ( manyuser liked  the posts ) exists in  all Liked posts
+            const index = post.likes.indexOf(req.user._id);     // find specific index of that place 
+
+            post.likes.splice(index,1);                 // remove it from that place 
+
+            return res.status(200).json({
+                success  :true,
+                message : "Post Unliked",
+            });
+        }else{
+            // like the post now
+            post.likes.push(req.user._id);
+            await post.save();
+
+            return res.status(200).json({
+                success : true,
+                message :  " Post Liked "
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+             success  :false,
+             message : error.message
         })
     }
 }
--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
-const { CreatePost } = require('../controllers/PostController.js');
+const { CreatePost, LikeandUnlikePost } = require('../controllers/PostController.js');
 const { isAuthenticated } = require('../middlewares/auth.js');
 
 const router = express.Router();
 
 router.route('/post/upload').post(isAuthenticated,CreatePost);
+
+
+// :id -  specific post id should be given 
+router.route('/post/:id').get(isAuthenticated,LikeandUnlikePost);
 
 module.exports = router;
