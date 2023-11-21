cd87ba4d45e921824076b2307611b6948c222a34
SARB <mrsinghbusiness05@gmail.com>
2023-11-21 07:10:42 +0530

    Followers/Following - backend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -66,3 +66,67 @@ exports.Login = async(req,res) => {
         })
     }
 }
+
+//Follow User 
+exports.FollowUser = async(req,res) => {
+    try {
+        const userToFollow = await User.findById(req.params.id);    // user i need to follow
+        const loggedInUser   = await User.findById(req.user._id);       //logged User
+
+        // if user we want to follow not existed 
+        if(!userToFollow){
+            return res.status(404).json({
+                success : false,
+                message : " User not Found ",
+            });
+        }
+
+        // If usertoFollow existed then //
+        // two cases can be there - 1) - May be user is  already followed then unfolow it
+        // 2 )- if not Followed then Follow it normally 
+
+
+
+        // 1) case - check if user is already followed
+        if(loggedInUser.following.includes(userToFollow._id)){
+            // checked in following 
+           const indexFollowing = loggedInUser.following.indexOf(userToFollow._id);          // If user to follow already exist then 
+
+           loggedInUser.following.splice(indexFollowing,1);        // id find  in loggeduser  then remove it
+
+           // check in Followers
+           const indexFollowers = userToFollow.followers.indexOf(loggedInUser._id);         // if id find get the index 
+            userToFollow.followers.splice(indexFollowers,1);                        // remove id from  here also 
+
+            // From  Both sides if id Existed then remove it and save it
+
+             await loggedInUser.save();
+             await userToFollow.save();
+
+             res.status(200).json({
+                 success : true,
+                 message : " User UnFollowed "
+             })
+
+        }else{   
+            // 2) case - if existed then Follow it 
+            loggedInUser.following.push(userToFollow._id);            // login user  following increased +1 when someonefollowed it 
+            userToFollow.followers.push(loggedInUser._id);            // now user we have followed  increased by + 1
+            
+            await loggedInUser.save();
+            await userToFollow.save();
+            
+            res.status(200).json({
+                success : true,
+                message : " User Followed "
+            })
+        }
+
+
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : error.message
+        })  
+    }
+}
--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { Register, Login } = require('../controllers/UserController');
+const { Register, Login, FollowUser } = require('../controllers/UserController');
+const { isAuthenticated } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/register').post(Register);
 
 router.route('/login').post(Login);
+
+router.route('/follow/:id').get(isAuthenticated,FollowUser);
 
 module.exports = router;
