517c1223dbab733993364a030779f9938ee067bd
SARB <mrsinghbusiness05@gmail.com>
2023-12-02 16:25:18 +0530

    backend origin changed

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ app.use(express.json({
 app.use(cors());
 
 app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', 'https://social-media-backend-33gv.onrender.com/');
     res.header('Access-Control-Allow-Credentials', true);
     next();
 })
