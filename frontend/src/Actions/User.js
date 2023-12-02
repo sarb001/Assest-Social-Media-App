48507a10e734b7240e93f99481d04e604646d8cc
SARB <mrsinghbusiness05@gmail.com>
2023-12-02 11:36:01 +0530

    backend url - added

--- frontend/src/Actions/User.js
+++ frontend/src/Actions/User.js
@@ -4,7 +4,7 @@ export const userRegister = (name,email,password,avatar) => async(dispatch) => {
     try {
         dispatch({type:"RegisterRequest"});
         
-        const {data} = await axios.post('/api/v1/register' , 
+        const {data} = await axios.post('https://social-media-backend-33gv.onrender.com/api/v1/register' , 
         { name,email,password , avatar },
         {
             headers : {
@@ -25,7 +25,7 @@ export const  loginUser = (email,password) => async(dispatch) => {
          dispatch({type:"LoginRequest"});
          console.log('data started  -');
 
-        const { data } = await  axios.post('/api/v1/login', {email,password},
+        const { data } = await  axios.post('https://social-media-backend-33gv.onrender.com/api/v1/login', {email,password},
         { headers : 
               {
                 "Content-Type" : 'application/json'
@@ -46,7 +46,7 @@ export const loaduser = () => async(dispatch) => {
     try {
          dispatch({type:"LoadUserRequest"});
 
-         const { data } = await  axios.get('/api/v1/myprofile');
+         const { data } = await  axios.get('https://social-media-backend-33gv.onrender.com/api/v1/myprofile');
 
          dispatch({type:"LoadUserSuccess", payload : data.user})
     } catch (error) {
@@ -59,7 +59,7 @@ export const GetFollowingPostRequest = () => async(dispatch) => {
     try {
         dispatch({type:"GetFollowingPostRequest"});
         
-        const { data } = await axios.get('/api/v1/followpost');
+        const { data } = await axios.get('https://social-media-backend-33gv.onrender.com/api/v1/followpost');
         dispatch({type:"GetFollowingPostSuccess",payload : data.posts});
 
     } catch (error) {
@@ -72,7 +72,7 @@ export const GetAllUsers = (name = "") => async(dispatch) => {
     try {
         dispatch({type:"GetgetAllUsersRequest"})
 
-        const {data} = await axios.get(`/api/v1/users?name=${name}`);
+        const {data} = await axios.get(`https://social-media-backend-33gv.onrender.com/api/v1/users?name=${name}`);
 
         dispatch({type:"GetgetAllUsersSuccess",payload : data.users});
         
@@ -85,7 +85,7 @@ export const GetlikePost = (id) => async(dispatch) => {
     try {
         dispatch({type:"LikeRequest"});
         
-        const {data} = await axios.get(`/api/v1/post/${id}`)
+        const {data} = await axios.get(`https://social-media-backend-33gv.onrender.com/api/v1/post/${id}`)
 
         dispatch({type:"LikeSuccess",payload : data.message })
     } catch (error) {
@@ -97,7 +97,7 @@ export const GetMyPost = () => async(dispatch) => {
     try {
         dispatch({type: "GetPostRequest"});
 
-        const { data } = await axios.get('/api/v1/my/posts')
+        const { data } = await axios.get('https://social-media-backend-33gv.onrender.com/api/v1/my/posts')
 
         dispatch({type: "GetPostSuccess",payload : data.posts})
     } catch (error) {
@@ -109,7 +109,7 @@ export const GetMyPost = () => async(dispatch) => {
 export const GetComments = (id,comment) => async(dispatch) => {
     try {
         dispatch({type:"CommentRequest"}) 
-        const  { data } = await axios.put(`/api/v1/posts/comment/${id}` , {
+        const  { data } = await axios.put(`https://social-media-backend-33gv.onrender.com/api/v1/posts/comment/${id}` , {
             comment
         },{
             headers : {
@@ -126,7 +126,7 @@ export const GetDeleteComment = (id,commentId) => async(dispatch) => {
     try {
         dispatch({type:"DeleteCommentRequest"})
 
-        const { data } = await axios.delete(`/api/v1/posts/comment/${id}` , {
+        const { data } = await axios.delete(`https://social-media-backend-33gv.onrender.com/api/v1/posts/comment/${id}` , {
             data : { commentId },
         });
         
@@ -141,7 +141,7 @@ export const LogoutUser = () => async(dispatch) => {
     try {
         dispatch({type:"LogOutRequest"});
         
-        await axios.get('/api/v1/logout');
+        await axios.get('https://social-media-backend-33gv.onrender.com/api/v1/logout');
 
         dispatch({type:"LogOutSuccess"})
     } catch (error) {
@@ -153,7 +153,7 @@ export const LogoutUser = () => async(dispatch) => {
 export const CreateNewPost = (caption,image) => async(dispatch) => {
     try {
           dispatch({type:"NewPostRequest"});
-            const { data } = await axios.post(`/api/v1/post/upload`, 
+            const { data } = await axios.post(`https://social-media-backend-33gv.onrender.com/api/v1/post/upload`, 
             {
                 caption , image,
             },
@@ -174,7 +174,7 @@ export const UpdateCaption = (caption,id) => async(dispatch) => {
     try {
             dispatch({type:"UpdateCaptionRequest"});
 
-            const { data } = await axios.put(`/api/v1/post/${id}` , {
+            const { data } = await axios.put(`https://social-media-backend-33gv.onrender.com/api/v1/post/${id}` , {
                 caption
             },{
                 headers: {
@@ -193,7 +193,7 @@ export const DeletePost = (id) => async(dispatch) => {
     try {
         dispatch({type:"DeletePostRequest"});
         
-        const { data } = await axios.delete(`/api/v1/post/${id}`)
+        const { data } = await axios.delete(`https://social-media-backend-33gv.onrender.com/api/v1/post/${id}`)
 
         dispatch({type:"DeletePostSuccess" , payload : data.message });
     } catch (error) {
@@ -207,7 +207,7 @@ export const GetUpdatedProfile = (name,email,avatar) =>  async(dispatch) => {
     try {
         dispatch({type:"updateProfileRequest"});
 
-        const { data } = await axios.put('/api/v1/update/profile' , 
+        const { data } = await axios.put('https://social-media-backend-33gv.onrender.com/api/v1/update/profile' , 
         { name,email,avatar },
         {
                 headers : {
@@ -226,7 +226,7 @@ export const DeleteProfile = () => async(dispatch) => {
     try {
         dispatch({type:"DeleteProfileRequest"});
 
-        const {data} = await axios.delete('/api/v1/delete/me');
+        const {data} = await axios.delete('https://social-media-backend-33gv.onrender.com/api/v1/delete/me');
 
         dispatch({type:"DeleteProfileSuccess",payload: data.message });
 
@@ -239,7 +239,7 @@ export const DeleteProfile = () => async(dispatch) => {
 export const GetUserProfile = (id) => async(dispatch) => {
     try {
          dispatch({type:"GetUserProfileRequest"});
-         const {data} = await axios.get(`/api/v1/user/${id}`)
+         const {data} = await axios.get(`https://social-media-backend-33gv.onrender.com/api/v1/user/${id}`)
 
          dispatch({type:"GetUserProfileSuccess" , payload : data.user});
         } catch (error) {
@@ -250,7 +250,7 @@ export const GetUserProfile = (id) => async(dispatch) => {
 export const GetUserPost = (id) =>  async(dispatch) => {
     try {
         dispatch({type:"GetUserPostRequest"});
-        const {data} = await axios.get(`/api/v1/userposts/${id}`)
+        const {data} = await axios.get(`https://social-media-backend-33gv.onrender.com/api/v1/userposts/${id}`)
 
         dispatch({type:"GetUserPostSuccess" , payload : data.posts});
        } catch (error) {
@@ -262,7 +262,7 @@ export const GetUserPost = (id) =>  async(dispatch) => {
 export const FollowandUnfollowUser = (id) =>  async(dispatch) => {
     try {
         dispatch({type:"FollowandUnfollowRequest"});
-        const {data} = await axios.get(`/api/v1/follow/${id}`)
+        const {data} = await axios.get(`https://social-media-backend-33gv.onrender.com/api/v1/follow/${id}`)
 
         dispatch({type:"FollowandUnfollowSuccess" , payload : data.message});
        } catch (error) {
