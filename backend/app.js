bbaf14b7ab1e39b69d041dbaf6b50f3c72fddf06
SARB <mrsinghbusiness05@gmail.com>
2023-12-02 13:48:55 +0530

    using cors header - backend

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,14 @@ app.use(express.json({
     limit : '50mb'
 }))
 
+app.use(cors());
 
-app.use(cors({
-    methods : 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials : true,
-    origin : 'https://assest-social-media-app.vercel.app/'
-}));
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', 'https://assest-social-media-app.vercel.app/');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+})
+
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
