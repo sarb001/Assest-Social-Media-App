4cdafce56cb6713b89e384d8fe2802679a529711
SARB <mrsinghbusiness05@gmail.com>
2023-12-03 02:59:12 +0530

    backend url added

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json({
 }))
 
 app.use(cors({
-    // origin : 'https://assest-social-media-app.vercel.app',
+    origin : 'https://assest-social-media-app.vercel.app',
     credentials : true,
 }));
 
