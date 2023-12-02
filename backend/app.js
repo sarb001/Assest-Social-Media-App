748562842dadf3aacd89b3ba379fc44983279938
SARB <mrsinghbusiness05@gmail.com>
2023-12-02 10:34:09 +0530

    cors adde

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const  cookieParser = require('cookie-parser');
 const path = require('path');
+const cors = require('cors');
 
 app.use(express.json({
     limit : '50mb'
 }))
 
+app.use(cors());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
