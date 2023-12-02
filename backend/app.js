f51e5e5f0e3c3a8f5bb3a71463e5c6e6dfa870ac
SARB <mrsinghbusiness05@gmail.com>
2023-12-02 11:43:12 +0530

    backend error removed

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const corsOptions = {
     credentials : true,
 }
 
-app.use(cors(corsOptions()));
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
