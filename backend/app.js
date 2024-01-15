d3d021cee5516fe495dda28e5a2c52b649560a61
SARB <mrsinghbusiness05@gmail.com>
2024-01-15 07:57:52 +0530

    adding cors - production

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,14 @@ const cors = require('cors');
 app.use(express.json({
     limit : '50mb'
 }))
-
-
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
+
+app.use(cors({
+     origin : process.env.FRONTEND_URL,
+     credentials: true,
+     methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 
 
 // importing Routes
