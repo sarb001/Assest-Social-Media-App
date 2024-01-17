bab98e8ba60990e3959fb60a6155d8997eeefe56
SARB <mrsinghbusiness05@gmail.com>
2024-01-17 15:08:36 +0530

    correcting app.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -71,7 +71,7 @@ exports.Login = async(req,res) => {
 
         const  token = await user.generateToken();
         res.cookie("token",token , {
-            Domain :  '.vercel.com',
+            Domain :  '.vercel.app',
             secure  :  true,
             expires  : new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
             httpOnly : true,
