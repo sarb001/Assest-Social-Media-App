860b98e417e23420af5d4f554dbe79ca6ac51e9e
SARB <mrsinghbusiness05@gmail.com>
2024-02-13 07:28:02 +0530

    domain changed/auth

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());    
 
 app.use(cors({
-     origin : process.env.FRONTEND_URL,
+     origin : true,
      credentials: true,
      methods: ["GET", "POST", "PUT", "DELETE"],
 }))
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,14 +6,16 @@ const jwt = require('jsonwebtoken');
 exports.isAuthenticated = async(req,res,next) => {
     try {   
         console.log('cookies req--',req);
-       const { token } = req.cookies;
-       console.log('token in cookies -',token);
+        const { token } = req.cookies;
+        console.log('token in cookies -',token);
 
         if(!token){
           return res.status(401).json({ message: " No Token Present " });
         }
+        res.status(200).json({token});
         const decoded =  jwt.verify(token,process.env.SECRET_KEY);
         req.user = await  User.findById(decoded._id);
+
         next();
 
 } catch (error) {
