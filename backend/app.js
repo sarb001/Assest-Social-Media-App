7736571e487c255b3499ac577b4fd3e03c80c1be
SARB <mrsinghbusiness05@gmail.com>
2024-02-15 10:50:25 +0530

    cors changes -app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ app.use(express.json({
     limit : '50mb'
 }))
 app.use(express.urlencoded({extended : true}));
-app.use(cookieParser());
+app.use(cookieParser());    
+
+app.use(cors())
 
 
 // importing Routes
