6dac801df851fa0b3e327fd4df3d1d5042579240
SARB <mrsinghbusiness05@gmail.com>
2024-02-15 10:46:17 +0530

    cors for all

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 app.use(express.json({
     limit : '50mb'
 }))
-
-
 app.use(express.urlencoded({extended : true}));
-app.use(cookieParser());
+app.use(cookieParser());    
+
+app.use(cors())
 
 
 // importing Routes
@@ -20,10 +20,6 @@ const user = require('./routes/User.js');
 // Defining route  path 
 app.use('/api/v1' , post);
 app.use('/api/v1' , user);
-
-app.get('/' ,(req,res)  => {
-    res.send('Helllo Backend Here Working ');
-})
 
 
 
