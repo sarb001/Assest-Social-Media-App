3966cd521252ba75c4a85541ab109843c856e9ac
SARB <mrsinghbusiness05@gmail.com>
2024-02-24 05:07:48 +0530

    added limit- extended

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(express.json({
     limit : '50mb'
 }))
 
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(cookieParser());    
 
 app.use(cors({
